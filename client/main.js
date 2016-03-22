88d17a038862ef2e2ac8435bfadeff1098af2b56
Maciek Stasieluk <macrusher@gmail.com>
2016-03-22 03:11:43 +0100

    Basic demo

---
+++ client/main.js
@@ -0,0 +1,25 @@
+import {Meteor} from 'meteor/meteor';
+
+import Cycle from '@cycle/core';
+import {div, label, input, hr, h1, makeDOMDriver} from '@cycle/dom';
+
+function main(sources) {
+    const sinks = {
+        DOM: sources.DOM.select('.field').events('input')
+            .map(ev => ev.target.value)
+            .startWith('')
+            .map(name =>
+                div([
+                    label('Name:'),
+                    input('.field', {attributes: {type: 'text'}}),
+                    hr(),
+                    h1('Hello ' + name),
+                ])
+            )
+    };
+    return sinks;
+}
+
+Meteor.startup(() => {
+    Cycle.run(main, {DOM: makeDOMDriver('#app')});
+});
